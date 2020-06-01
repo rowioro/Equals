6aa8a37b0e4d6df86d3e88e08857a4a75185b143
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 18:49:06 +0200

    Add project files.

---
+++ ConsoleApp13_Equals/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string m1 = "Warszawa";
+            //String m2 = new string("Warszawa".ToArray());
+            //Console.WriteLine(m1.Equals(m2));
+
+            Auto auto1 = new Auto("Toyota", 1995);
+            Auto auto2 = new Auto("Toyota", 1995);
+            Console.WriteLine(auto1.Equals(auto2));
+
+            Console.ReadKey();
+        }
+    }
+
+    class Auto
+    {
+        public string Marka { get; set; }
+        public int Rocznik { get; set; }
+
+        public Auto(string Marka, int Rocznik)
+        {
+            this.Marka = Marka;
+            this.Rocznik = Rocznik;
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    return base.Equals(obj);
+        //}
+
+        public bool Equals(Auto auto)
+        {
+            return auto.Marka == this.Marka && auto.Rocznik==this.Rocznik;
+        }
+    }
+}
